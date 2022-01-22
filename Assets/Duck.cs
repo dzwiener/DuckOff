45eda55f0b12fe5b8449b09f7df59da377782be3
Blake Erquiaga <cbcerquiaga@me.com>
2022-01-22 11:01:29 -0500

    Random Bounce Collision
    The duck and human can collide, and if they do they bounce randomly off of one another. Alternatively, we can just prevent them from moving into the same space, but I think this way could be sillier and fit the vibe better

--- Assets/Duck.cs
+++ Assets/Duck.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Duck : Player
 {
+    Human human;
     // Start is called before the first frame update
     void Start()
     {
-
+      var humanObject = GameObject.Find("HUMAN");
+      human = humanObject.GetComponent<Human>();
     }
 
     // Update is called once per frame
@@ -16,6 +18,7 @@ public class Duck : Player
       if (Input.GetKeyDown(KeyCode.W)){
         //move up
         //transform.position = transform.position + new Vector2(0,1);//TODO
+        //if (human.getPosition().x != transform.position.x + 1)
         transform.position = transform.position + new Vector3(0,1,0);//TODO
       } else if (Input.GetKeyDown(KeyCode.S)){
         //move down
@@ -30,6 +33,9 @@ public class Duck : Player
         //move right
         //transform.position = transform.position + new Vector2(1,0);
         transform.position = transform.position + new Vector3(1,0,0);//TODO
+      }
+      if (touching(human.getPosition())){
+        bounce(human.getPosition());
       }
     }
 
--- Assets/Human.cs
+++ Assets/Human.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Human : Player
 {
+    Duck duck;
     // Start is called before the first frame update
     void Start()
     {
-
+      var duckObject = GameObject.Find("DUCK");
+      duck = duckObject.GetComponent<Duck>();
     }
 
     // Update is called once per frame
@@ -15,21 +17,20 @@ public class Human : Player
     {
       if (Input.GetKeyDown(KeyCode.UpArrow)){
         //move up
-        //transform.position = transform.position + new Vector2(0,1);//TODO
-        transform.position = transform.position + new Vector3(0,1,0);//TODO
+        transform.position = transform.position + new Vector3(0,1,0);
       } else if (Input.GetKeyDown(KeyCode.DownArrow)){
         //move down
-        // transform.position = transform.position + new Vector2(0,-1);
-        transform.position = transform.position + new Vector3(0,-1,0);//TODO
+        transform.position = transform.position + new Vector3(0,-1,0);
       }
       if (Input.GetKeyDown(KeyCode.LeftArrow)){
         //move left
-        // transform.position = transform.position + new Vector2(-1,0);
-        transform.position = transform.position + new Vector3(-1,0,0);//TODO
+        transform.position = transform.position + new Vector3(-1,0,0);
       } else if (Input.GetKeyDown(KeyCode.RightArrow)){
         //move right
-        //transform.position = transform.position + new Vector2(1,0);
-        transform.position = transform.position + new Vector3(1,0,0);//TODO
+        transform.position = transform.position + new Vector3(1,0,0);
+      }
+      if (touching(duck.getPosition())){
+        bounce(duck.getPosition());
       }
     }
 
